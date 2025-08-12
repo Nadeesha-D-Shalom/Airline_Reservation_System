8309c812f799dde9e15f9a3921dc10e35b332d91
Nadeesha-D-Shalom <nadeeshashalom1@gmail.com>
2025-08-12 20:34:49 +0530

    Initial commit: Spring Boot app

---
+++ airlineReservationApp/airlineReservationApp/src/main/java/com/example/airlineReservationApp/AirlineReservationAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.airlineReservationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineReservationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineReservationAppApplication.class, args);
+	}
+
+}
---
+++ airlineReservationApp/airlineReservationApp/src/test/java/com/example/airlineReservationApp/AirlineReservationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airlineReservationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineReservationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
